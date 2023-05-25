d36de26dcd99d9aeb2a2c60063a72fe20d7ab9c7
Ogbo Friday <hedosphere@gmail.com>
2023-05-25 18:34:43 +0100

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import MyThemeProvider from "./MyThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Header />
           {/*  header nav*/}
 
+          <Navbar />
           {/* nav bar */}
 
           {/*search  */}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import MenuItem from "./MenuItem";
 import { AiTwotoneHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
-import ColorMode from "./ColorMode";
+import ColorModeSwitch from "./ColorMode";
 //
 
 export const Header = () => {
   return (
-    <div className=" py-3  select-none  ">
+    <div className=" py-8  select-none  ">
       {/* <div className=" py-3  select-none border-b border-gray-700  border-opacity-10 dark:border-white  "> */}
       <div className="max-w-[90vw] mx-auto items-center flex justify-between gap-4">
         <div className="flex gap-6">
@@ -20,7 +20,7 @@ export const Header = () => {
         {/*  */}
         <div className="flex items-center gap-4">
           <>
-            <ColorMode />
+            <ColorModeSwitch />
           </>
           <div>
             <span className="bg-amber-500 rounded-lg p-1  mr-1 text-2xl">
