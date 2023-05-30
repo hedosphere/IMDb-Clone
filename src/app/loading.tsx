661a280a2bd0153ba09ed0e5d6798af90709a27f
Ogbo Friday <hedosphere@gmail.com>
2023-05-30 17:23:49 +0100

    Update Loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 const loading = () => {
-  return <div className="h-full w-full bg-red-500">loading...</div>;
+  return (
+    <div className="h-[60vh] w-full flex items-center justify-center bg-transparent ">
+      <Image
+        src={"/Loading_Spinner.svg"}
+        height={500}
+        width={500}
+        alt="Loading..."
+      />
+    </div>
+  );
 };
 
 export default loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 export default async function Home({ searchParams }: any) {
   const { genre } = searchParams;
 
+  // upcoming popular top_rated
   const TMDBurl = `https://api.themoviedb.org/3/movie/${
     genre == "fetchTopRated" ? "top_rated" : "popular"
   }?api_key=${TMDB_API_KEY}&language=en-US&page=1`;
 
-  let res = await fetch(TMDBurl, { next: { revalidate: 21400 } });
+  let res = await fetch(TMDBurl, { next: { revalidate: 1 } });
 
   if (!res.ok) throw new Error("Fail to load new movies");
 
